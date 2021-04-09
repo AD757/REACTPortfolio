528642474cae9299e178d1f43050b619a08f31f4
AD757 <alladin.daher@hotmail.com>
2021-04-10 00:54:07 +0200

    added contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,53 +1,21 @@
 import React, { Component } from 'react'
-import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl'
+import { Grid, Cell } from 'react-mdl'
+import ContactForm from './contactForm'
 
 class Contact extends Component {
   render() {
     return (
-      <div className="contact-body">
-        <Grid className="contact-grid">
-          <Cell col={6} tablet={8} phone={12}>
-            <h3>Alladin Daher</h3>
-            <img
-              src="https://i.imgur.com/3NHO4Uv.png"
-              alt="cv-cover"
-              style={{ height: '285px', borderRadius: '10px', margin: '50px' }}
-            />
-            <p style={{ width: '80%', margin: 'auto' }}>
-              Looking for a passionate, dedicated and creative Frontend Web Developer?
-              Contact me for more information on how I can help your team.
-            </p>
-          </Cell>
-          <Cell col={6} tablet={8} phone={12}>
-            <h2 style={{ paddingTop: '40px' }}>Contact Me</h2>
-            <hr />
-            <div className="contact-list">
-              <List>
-                <ListItem>
-                  <ListItemContent style={{ fontSize: '20px', fontFamily: 'Poppins' }}>
-                    <i className="fa fa-phone-square" aria-hidden="true" />
-                    <a href="tel:+46737152378" target="_blank" rel="noopener noreferrer">
-                      (+46) 73 715 23 78
-                    </a>
-                  </ListItemContent>
-                </ListItem>
-
-                <ListItem>
-                  <ListItemContent style={{ fontSize: '20px', fontFamily: 'Poppins' }}>
-                    <i className="fas fa-envelope" aria-hidden="true" />
-                    <a
-                      href="mailto:alladin.daher@hotmail.com"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      alladin.daher@hotmail.com
-                    </a>
-                  </ListItemContent>
-                </ListItem>
-              </List>
-            </div>
-          </Cell>
-        </Grid>
+      <div className="contact-body" style={{ marginTop: '30px' }}>
+        <div class="container con2"></div>
+        <div id="contact">
+          <br />
+          <br />
+          <Grid className="contact-grid">
+            <Cell col={6} tablet={12} phone={12}>
+              <ContactForm />
+            </Cell>
+          </Grid>
+        </div>
       </div>
     )
   }
---
+++ src/components/contactForm.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+
+class ContactForm extends Component {
+  render() {
+    return (
+      <div>
+        <h2 className="h2-contact">Contact me</h2>
+        <br />
+        <img
+          src="https://i.imgur.com/3NHO4Uv.png"
+          alt="cv-cover"
+          style={{ height: '200px', margin: 'auto' }}
+        />
+        <br />
+        <br />
+        <p style={{ width: '80%', margin: 'auto', fontSize: '18px' }}>
+          Looking for a passionate, dedicated and creative Frontend Web Developer? Contact
+          me for more information on how I can help your team.
+        </p>
+        <br />
+        <form action="#" method="#">
+          <label className="formLabel" htmlFor="">
+            Name:
+          </label>
+          <input type="text" className="contactInput" />
+          <br />
+          <br />
+          <label className="formLabel" htmlFor="">
+            Email:
+          </label>
+          <input type="text" className="contactInput" />
+          <br />
+          <br />
+          <label className="formLabel" htmlFor="">
+            Phone:
+          </label>
+          <input type="text" className="contactInput" style={{ marginLeft: '-6px' }} />
+          <br />
+          <br />
+          <label className="formLabel1" htmlFor="">
+            Message:
+            <br />
+          </label>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            className="textArea1"
+            style={{
+              marginTop: '7px',
+              border: '1.5px solid #000',
+              borderRadius: '8px',
+              padding: '0 105px',
+            }}
+          />
+          <br />
+          <br />
+          <button
+            style={{
+              padding: '10px 65px',
+              borderRadius: '5px',
+              backgroundColor: '#26AEDF',
+              color: 'white',
+              fontSize: '16px',
+            }}
+          >
+            Send
+          </button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default ContactForm
